6f60a4786ffa719fd7eb8321388caad905c4c649
Shawn Walker <walkersw@users.noreply.github.com>
2020-12-10 10:09:14 -0600

    continue setting up...

---
+++ ahf/__init__.py
@@ -0,0 +1,6 @@
+"""
+ahf
+~~~~~~
+The ahf package - a Python package for the Array-based Half-facet data structure.
+It is a mesh class.
+"""
---
+++ ahf/__version__.py
@@ -0,0 +1,13 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+Information about the current version of the ahf package.
+"""
+
+__title__ = 'ahf-walkersw'
+__description__ = 'Python Class for the AHF mesh data structure'
+__version__ = '0.0.1'
+__author__ = 'Shawn W. Walker'
+__author_email__ = 'walker@lsu.edu'
+__license__ = 'GNU GENERAL PUBLIC LICENSE'
+__url__ = 'https://github.com/walkersw/PyAHF'
