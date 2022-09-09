42058cc402d5d274669708387f4dce496d7ed9ea
Shawn Walker <walkersw@users.noreply.github.com>
2022-09-09 08:57:22 -0500

    start adding simplex math.

--- ahf/SimplexMath.py
+++ ahf/SimplexMath.py
@@ -17,34 +17,137 @@ from ahf import *
 
 
 def Affine_Map(vtx_coord):
-    """Get the (jacobian) matrix and translation vector for the affine map
-    from the 'standard' reference simplex. This is the usual finite element
-    affine map.
-    Input: vtx_coord is a (TD+1,GD) numpy array that gives the coordinates of
-    the vertices of a simplex of topological dimension TD embedded in a
-    Euclidean space of dimension GD.
+    """Get the (jacobian) matrix and translation vector for the affine map from the
+    'standard' reference simplex. This is the usual finite element affine map.
+    There are two ways to call this function:
+    Input: vtx_coord: a (TD+1,GD) numpy array that gives the coordinates of the
+           vertices of a single simplex of topological dimension TD embedded in
+           a Euclidean space of dimension GD.
+    Outputs: A is the jacobian matrix of shape (GD,TD);
+             b is the translation vector of shape (GD,1).
+    OR
+    Input: vtx_coord: a (M,TD+1,GD) numpy array that gives the coordinates of the
+           vertices of M simplices of topological dimension TD embedded in
+           a Euclidean space of dimension GD.
+    Outputs: A contains M jacobian matrices, with shape (M,GD,TD);
+             b contains M translation vectors of shape (M,GD,1).
     """
     if type(vtx_coord) is not np.ndarray:
         print("Error: input must be a numpy array!")
         return
 
+    ndim   = vtx_coord.ndim
     dim_vc = vtx_coord.shape
-    if len(dim_vc)==1:
-        print("Error: input must be a numpy array with a 2-D shape!")
+    if ndim==1:
+        print("Error: input must be a numpy array of shape (TD+1,GD) or (M,TD+1,GD)!")
         return
-    else:
+    elif ndim==2:
         TD = dim_vc[0]-1
         GD = dim_vc[1]
 
-    # translation vector
-    b = np.zeros((GD,1), dtype=CoordType)
-    b[:] = (vtx_coord[[0],:]).T
-    
-    # Jacobian matrix
-    A = np.zeros((GD,TD), dtype=CoordType)
-    for tt in np.arange(1, TD+1, dtype=SmallIndType):
-        # special case of a linear simplex:
-        A[:,[tt-1]] = (vtx_coord[[tt],:]).T - b
+        # translation vector
+        b = np.zeros((GD,1), dtype=CoordType)
+        b[:] = (vtx_coord[[0],:]).T
+        
+        # Jacobian matrix
+        A = np.zeros((GD,TD), dtype=CoordType)
+        for tt in np.arange(1, TD+1, dtype=SmallIndType):
+            # special case of a linear simplex:
+            A[:,[tt-1]] = (vtx_coord[[tt],:]).T - b
+    else:
+        M  = dim_vc[0]
+        TD = dim_vc[1]-1
+        GD = dim_vc[2]
+
+        # translation vectors
+        b = np.zeros((M,GD,1), dtype=CoordType)
+        b[:,:,0] = vtx_coord[:,0,:]
+
+        # Jacobian matrices
+        A = np.zeros((M,GD,TD), dtype=CoordType)
+        for tt in np.arange(1, TD+1, dtype=SmallIndType):
+            # special case of a linear simplex:
+            A[:,:,(tt-1)] = vtx_coord[:,tt,:] - b[:,:,0]
 
     return A, b
+
+def Reference_To_Cartesian(vtx_coord, ref_coord):
+    """Convert reference element coordinates to cartesian coordinates, where the reference
+    element is the "standard" reference simplex.
+    There are two ways to call this function:
+    Inputs: vtx_coord: a (TD+1,GD) numpy array that gives the coordinates of the
+            vertices of a single simplex of topological dimension TD embedded in
+            a Euclidean space of dimension GD;
+            ref_coord: a (TD,) numpy array that gives the coordinates of a point in the
+            reference simplex.
+    Output: (GD,) numpy array that gives the cartesian coordinates of the given point.
+    OR
+    Inputs: vtx_coord: a (M,TD+1,GD) numpy array that gives the coordinates of the
+            vertices of M simplices of topological dimension TD embedded in
+            a Euclidean space of dimension GD;
+            ref_coord: a (M,TD) numpy array that gives the coordinates of M points in the
+            reference simplex.
+    Output: (M,GD) numpy array that gives the cartesian coordinates of the given M points.
+    """
+    if type(vtx_coord) is not np.ndarray:
+        print("Error: vtx_coord must be a numpy array!")
+        return
+    if type(ref_coord) is not np.ndarray:
+        print("Error: ref_coord must be a numpy array!")
+        return
+
+    ndim   = vtx_coord.ndim
+    dim_vc = vtx_coord.shape
+    if ndim==1:
+        print("Error: vtx_coord must be a numpy array of shape (TD+1,GD) or (M,TD+1,GD)!")
+        return
+    elif ndim==2:
+        TD = dim_vc[0]-1
+        GD = dim_vc[1]
+
+        dim_rc = ref_coord.shape
+        if dim_rc[0]!=TD:
+            print("Error: ref_coord must be a numpy array of shape (TD,) if vtx_coord is (TD+1,GD)!")
+            return
+
+        # note: this is computed using (effectively) barycentric coordinates
+
+        # compute 0th barycentric coordinate
+        BC0 = 1.0 - np.sum(ref_coord)
+
+        # init to 0th contribution
+        cart_coord = np.zeros((GD,), dtype=CoordType)
+        cart_coord[:] = BC0 * vtx_coord[0,:]
+        
+        for tt in np.arange(1, (TD+1), dtype=SmallIndType):
+            # add contribution (from other barycentric coordinates)
+            cart_coord[:] += ref_coord[tt-1] * vtx_coord[tt,:]
+
+    else:
+        M  = dim_vc[0]
+        TD = dim_vc[1]-1
+        GD = dim_vc[2]
+        
+        dim_rc = ref_coord.shape
+        if (dim_rc[0]!=M) or (dim_rc[1]!=TD):
+            print("Error: ref_coord must be a numpy array of shape (M,TD) if vtx_coord is (M,TD+1,GD)!")
+            return
+
+        # note: this is computed using (effectively) barycentric coordinates
+
+        # compute 0th barycentric coordinate
+        BC0 = 1.0 - np.sum(ref_coord, axis=1)
+
+        # init to 0th contribution
+        cart_coord = np.zeros((M,GD), dtype=CoordType)
+        cart_coord[:,:] = vtx_coord[:,0,:] * BC0[:, np.newaxis]
+        
+        for tt in np.arange(1, (TD+1), dtype=SmallIndType):
+            # add contribution (from other barycentric coordinates)
+            cart_coord[:,:] += vtx_coord[:,tt,:] * ref_coord[:, [tt-1]]
+
+    return cart_coord
+
+
+
 
--- unit_tests/SimplexMath_unittest.py
+++ unit_tests/SimplexMath_unittest.py
@@ -20,30 +20,66 @@ class TestSimplexMath(unittest.TestCase):
         print(" ")
 
     def test_Affine_Map(self):
-        vtx_coord = np.array([[0.0, 0.0, 1.1], [2.0, 3.0, 1.1], [0.0, 5.0, 1.1]])
+        vc0 = np.array([[0.0, 0.0, 1.1], [2.0, 3.0, 1.1], [0.0, 5.0, 1.1]])
 
-        print(vtx_coord)
-        A, b = Affine_Map(vtx_coord)
-        print(A)
-        print(b)
-        J_sq = np.matmul(A.T,A)
+        print(vc0)
+        A0, b0 = Affine_Map(vc0)
+        print(A0)
+        print(b0)
+        J_sq = np.matmul(A0.T,A0)
         J_sq_det = np.linalg.det(J_sq)
         J_det = np.round(np.sqrt(J_sq_det), 8)
         print(J_det)
         self.assertEqual(J_det, 10.0, "Should be 10.0.")
         
-        vtx_coord = np.array([[0.2, 0.3, -0.4], [1.2, 2.4, 0.5], [3.1, 0.7, 4.5]])
+        vc1 = np.array([[0.2, 0.3, -0.4], [1.2, 2.4, 0.5], [3.1, 0.7, 4.5]])
 
-        print(vtx_coord)
-        A, b = Affine_Map(vtx_coord)
-        print(A)
-        print(b)
-        J_sq = np.matmul(A.T,A)
+        print(vc1)
+        A1, b1 = Affine_Map(vc1)
+        print(A1)
+        print(b1)
+        J_sq = np.matmul(A1.T,A1)
         J_sq_det = np.linalg.det(J_sq)
         J_det = np.round(np.sqrt(J_sq_det), 8)
         print(J_det)
         self.assertEqual(J_det, 11.67155088, "Should be 11.67155088.")
 
+        vc2 = np.array([vc0, vc1])
+        A2, b2 = Affine_Map(vc2)
+        print(A2)
+        print(b2)
+        A2_CHK = np.array([A0, A1])
+        b2_CHK = np.array([b0, b1])
+        self.assertEqual(np.array_equal(A2_CHK,A2), True, "Should be True.")
+        self.assertEqual(np.array_equal(b2_CHK,b2), True, "Should be True.")
+
+    def test_Reference_To_Cartesian(self):
+        vc0 = np.array([[0.0, 0.0, 1.1], [2.0, 3.0, 1.1], [0.0, 5.0, 1.1]])
+        print(vc0)
+        rc0 = np.array([0.3, 0.2])
+        print(rc0)
+
+        cart0 = Reference_To_Cartesian(vc0, rc0)
+        print(cart0)
+        diff0 = np.amax(np.abs(cart0 - np.array([0.6, 1.9, 1.1], dtype=CoordType)))
+        self.assertEqual(diff0 < 1e-15, True, "Should be True.")
+        
+        vc1 = np.array([[0.2, 0.3, -0.4], [1.2, 2.4, 0.5], [3.1, 0.7, 4.5]])
+        print(vc1)
+        rc1 = np.array([0.1, 0.7])
+        print(rc1)
+
+        cart1 = Reference_To_Cartesian(vc1, rc1)
+        print(cart1)
+        diff1 = np.amax(np.abs(cart1 - np.array([2.33, 0.79, 3.12], dtype=CoordType)))
+        self.assertEqual(diff1 < 1e-15, True, "Should be True.")
+
+        vc2 = np.array([vc0, vc1])
+        rc2 = np.array([rc0, rc1])
+        cart2 = Reference_To_Cartesian(vc2, rc2)
+        #print(cart2)
+        cart2_CHK = np.array([cart0, cart1])
+        self.assertEqual(np.array_equal(cart2_CHK,cart2), True, "Should be True.")
 
 
 
